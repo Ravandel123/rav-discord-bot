43d37ce37ee1b5a29e7699fbbf99f590f949368a
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2018-09-06 21:06:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,24 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
  
-
 client.on('ready', () => {
 
     console.log('I am ready!');
 
 });
 
- client.on('message', message => {
+client.on('message', function (user, userID, channelID, message, evt) 
+{
+  if (message == 'h!') 
+  {
+    client.sendMessage({
+					to: channelID,
+					message: 'hey'
+				});
+  }
+		
+};
 
-    if (message.content == 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
 
 
 
