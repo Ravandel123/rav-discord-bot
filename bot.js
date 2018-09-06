bdb530a8fdcb2da65b053fb2d55d6520eacd896e
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2018-09-06 20:42:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 
  
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 2) == 'h!') 
 	{
         var args = message.substring(2).split(' ');
