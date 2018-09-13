6d95e2464b87c2503a5c5b125c32324397649efa
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2018-09-13 15:34:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -292,7 +292,7 @@ client.on("message", message => {
         }
         
 
-        message.channel.send(ReturnRandom(responseList) + ambushers + \n Will ' + who + ' survive? :scream:')
+        message.channel.send(ReturnRandom(responseList) + ambushers + '\n Will ' + who + ' survive? :scream:')
         break;
 
       //----------------------------------------------------------------------------------------------------------------------------
