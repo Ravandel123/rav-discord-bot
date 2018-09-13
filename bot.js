f1a314b5a6f65253b38ecd7320c602ca0ce57b82
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2018-09-13 11:06:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setActivity({game: {name: "Darkest Dungeon irl", type: 0}});
 });
 
 
