558fc0e73b6f158d902cca4d5b11b5a00c596249
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2018-11-11 20:30:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1186,8 +1186,434 @@ client.on("message", message => {
         case 'kidding':
           linkToPic = 'https://i.imgur.com/8lJPYe4.png';
           break;
+          
+        case 'bait':
+          linkToPic = 'https://i.imgur.com/cbqQp0Y.png';
+          break;
+          
+        case 'idontgetit':
+          linkToPic = 'https://i.imgur.com/rVzWUTn.png';
+          break;
+          
+        case 'jackie':
+          linkToPic = 'https://i.imgur.com/hbdTAl8.png';
+          break;
+          
+        case 'noob':
+          linkToPic = 'https://i.imgur.com/NU3XxPO.png';
+          break;
+          
+        case 'throw':
+          linkToPic = 'https://i.imgur.com/9Jnf496.png';
+          break;
+          
+        case 'dosomething':
+          linkToPic = 'https://i.imgur.com/b1wvo8w.png';
+          break;
+          
+        case 'concerned':
+          linkToPic = 'https://i.imgur.com/2wQTVwd.png';
+          break;
+          
+        case 'trollface':
+          linkToPic = 'https://i.imgur.com/RMHWix2.png';
+          break;
+          
+        case 'sad':
+          linkToPic = 'https://i.imgur.com/MznCnvD.png';
+          break;
+          
+        case 'megusta':
+          linkToPic = 'https://i.imgur.com/FogG79P.png';
+          break;
+          
+        case 'impossibru':
+          linkToPic = 'https://i.imgur.com/PRQABCC.png';
+          break;
+          
+        case 'bsmeter':
+          linkToPic = 'https://i.imgur.com/WulIVtE.png';
+          break;
+          
+        case 'nope':
+          linkToPic = 'https://i.imgur.com/jJTZIm0.png';
+          break;
+          
+        case 'cute':
+          linkToPic = 'https://i.imgur.com/BOKeJOG.png';
+          break;
+
+        case 'drakeyes':
+          linkToPic = 'https://i.imgur.com/5ZITUta.png';
+          break;
+          
+        case 'confused':
+          linkToPic = 'https://i.imgur.com/DVQvh46.png';
+          break;
+          
+        case 'seemslegit':
+          linkToPic = 'https://i.imgur.com/vCCXGeB.png';
+          break;
+          
+        case 'chuckok':
+          linkToPic = 'https://i.imgur.com/UZBv7Zc.png';
+          break;
+          
+        case 'pokerface':
+          linkToPic = 'https://i.imgur.com/zjmq40J.png';
+          break;
+          
+        case 'dude':
+          linkToPic = 'https://i.imgur.com/9FiB5OP.png';
+          break;
+          
+        case 'obvious':
+          linkToPic = 'https://i.imgur.com/CixYqts.png';
+          break;
+          
+        case 'iamout':
+          linkToPic = 'https://i.imgur.com/mHpudgf.png';
+          break;
+          
+        case 'fairenough':
+          linkToPic = 'https://i.imgur.com/HJi9wjU.png';
+          break;
+          
+        case 'facepalm':
+          linkToPic = 'https://i.imgur.com/eLUsk9d.png';
+          break;
+          
+        case 'goodjob':
+          linkToPic = 'https://i.imgur.com/F2n0KMt.png';
+          break;
+          
+        case 'yeah':
+          linkToPic = 'https://i.imgur.com/JA3doBb.png';
+          break;
+
+        case 'cool':
+          linkToPic = 'https://i.imgur.com/cVRVmyf.png';
+          break;
+          
+        case 'bplease':
+          linkToPic = 'https://i.imgur.com/kHV47Ou.png';
+          break;
+          
+        case 'wtf':
+          linkToPic = 'https://i.imgur.com/OV8c83R.png';
+          break;
+          
+        case 'arnielikes':
+          linkToPic = 'https://i.imgur.com/9d6X5R1.png';
+          break;
+          
+        case 'doitnow':
+          linkToPic = 'https://i.imgur.com/EckwoVF.png';
+          break;
+          
+        case 'what':
+          linkToPic = 'https://i.imgur.com/vsr0rjs.png';
+          break;
+
+        case 'mickey':
+          linkToPic = 'https://i.imgur.com/tR7VZE0.png';
+          break;
+          
+        case 'attack':
+          linkToPic = 'https://i.imgur.com/YjHRiJv.png';
+          break;
+          
+        case 'sinister':
+          linkToPic = 'https://i.imgur.com/9hlYrxY.png';
+          break;
+          
+        case 'drakeno':
+          linkToPic = 'https://i.imgur.com/w1kTtJA.png';
+          break;
+          
+        case 'xd':
+          linkToPic = 'https://i.imgur.com/Fyup5xA.png';
+          break;
+          
+        case 'smart':
+          linkToPic = 'https://i.imgur.com/VefDNlk.png';
+          break;
+
+        case 'mellikes':
+          linkToPic = 'https://i.imgur.com/LGCsLMV.png';
+          break;
+          
+        case 'parkerlikes':
+          linkToPic = 'https://i.imgur.com/K8Dyg6e.png';
+          break;
+          
+        case 'angry':
+          linkToPic = 'https://i.imgur.com/nkUiuSU.png';
+          break;
+          
+        case 'pulpfictionlikes':
+          linkToPic = 'https://i.imgur.com/ev7uVuY.png';
+          break;
+          
+        case 'trap':
+          linkToPic = 'https://i.imgur.com/sBDvybj.png';
+          break;
+          
+        case 'wall':
+          linkToPic = 'https://i.imgur.com/uqZPyK5.png';
+          break;
+
+        case 'prehistory':
+          linkToPic = 'https://i.imgur.com/ETPzsbH.png';
+          break;
+          
+        case 'throwanother':
+          linkToPic = 'https://i.imgur.com/u6IwHm9.png';
+          break;
+          
+        case 'krillin':
+          linkToPic = 'https://i.imgur.com/duiEzEQ.png';
+          break;
+          
+        case 'keanu':
+          linkToPic = 'https://i.imgur.com/LLnZqPk.png';
+          break;
+          
+        case 'dontwannalive':
+          linkToPic = 'https://i.imgur.com/IRbUTj2.png';
+          break;
+          
+        case 'charliecocaine':
+          linkToPic = 'https://i.imgur.com/OZqdJcg.png';
+          break;
+
+        case 'trollorstupid':
+          linkToPic = 'https://i.imgur.com/5jXnVLw.png';
+          break;
+          
+        case 'itsatrap':
+          linkToPic = 'https://i.imgur.com/VPvC9jk.png';
+          break;
+          
+        case 'nopower':
+          linkToPic = 'https://i.imgur.com/oS67pzi.png';
+          break;
+          
+        case 'bsintensifies':
+          linkToPic = 'https://i.imgur.com/OSNCumq.png';
+          break;
+          
+        case 'pasta':
+          linkToPic = 'https://i.imgur.com/gd3Lx1T.png';
+          break;
+          
+        case 'mythbusters':
+          linkToPic = 'https://i.imgur.com/lETdU4E.png';
+          break;
+
+        case 'xzibit':
+          linkToPic = 'https://i.imgur.com/bRXSiBb.png';
+          break;
+          
+        case '10score':
+          linkToPic = 'https://i.imgur.com/I9CTKlr.png';
+          break;
+          
+        case 'doublelikes':
+          linkToPic = 'https://i.imgur.com/ph18a49.png';
+          break;
+          
+        case 'genius':
+          linkToPic = 'https://i.imgur.com/PPx5BLM.png';
+          break;
+          
+        case 'sweating':
+          linkToPic = 'https://i.imgur.com/acWwV40.png';
+          break;
+          
+        case 'rambolikes':
+          linkToPic = 'https://i.imgur.com/XvJ83ih.png';
+          break;
+
+        case 'orbital':
+          linkToPic = 'https://i.imgur.com/gNucxNw.png';
+          break;
+
+        case 'crossover':
+          linkToPic = 'https://i.imgur.com/pQljlNR.png';
+          break;
+          
+        case 'offend':
+          linkToPic = 'https://i.imgur.com/2TkYGXE.png';
+          break;
+
+        case 'doesntaffect':
+          linkToPic = 'https://i.imgur.com/f96tYIG.png';
+          break;
+          
+        case 'propaganda':
+          linkToPic = 'https://i.imgur.com/eJPlUkk.png';
+          break;
+          
+        case 'babyyeah':
+          linkToPic = 'https://i.imgur.com/QKp0hq6.png';
+          break;
+          
+        case 'wrong':
+          linkToPic = 'https://i.imgur.com/Ea3ztjf.png';
+          break;
+          
+        case 'worsttrade':
+          linkToPic = 'https://i.imgur.com/pBkajpu.png';
+          break;
+          
+        case 'terminatorlikes':
+          linkToPic = 'https://i.imgur.com/edpMSyL.png';
+          break;
+
+        case 'dafuq':
+          linkToPic = 'https://i.imgur.com/fZPrp8Z.png';
+          break;
+
+        case 'beating':
+          linkToPic = 'https://i.imgur.com/clP1SJf.png';
+          break;
+          
+        case 'booze':
+          linkToPic = 'https://i.imgur.com/Hh58h6B.png';
+          break;
+
+        case 'mcmahon1':
+          linkToPic = 'https://i.imgur.com/wqSZgQN.png';
+          break;
+          
+        case 'mcmahon2':
+          linkToPic = 'https://i.imgur.com/VBJqzAJ.png';
+          break;
+          
+        case 'mcmahon3':
+          linkToPic = 'https://i.imgur.com/BTVb9Kq.png';
+          break;
+          
+        case 'adapt':
+          linkToPic = 'https://i.imgur.com/DlBYayG.png';
+          break;
+          
+        case 'evilestthing':
+          linkToPic = 'https://i.imgur.com/lWWgZ8s.png';
+          break;
+          
+        case 'laugh':
+          linkToPic = 'https://i.imgur.com/O1aCpTA.png';
+          break;
+
+        case 'summon':
+          linkToPic = 'https://i.imgur.com/Rpn7Q8e.png';
+          break;
+          
+        case 'shotgun':
+          linkToPic = 'https://i.imgur.com/qr1upDI.png';
+          break;
+          
+        case 'barbaric':
+          linkToPic = 'https://i.imgur.com/Ml1uB28.png';
+          break;
+        
+        case 'justdoit':
+          linkToPic = 'https://i.imgur.com/yE6Q5fv.png';
+          break;
+          
+        default:
+          linkToPic = 'Available pics!\n' +
+          '**kidding**\n'+
+          '**bait**\n'+
+          '**idontgetit**\n'+
+          '**jackie**\n'+
+          '**noob**\n'+
+          '**throw**\n'+
+          '**dosomething**\n'+
+          '**concerned**\n'+
+          '**trollface**\n'+
+          '**sad**\n'+
+          '**megusta**\n'+
+          '**impossibru**\n'+
+          '**bsmeter**\n'+
+          '**nope**\n'+
+          '**cute**\n'+
+          '**drakeyes**\n'+
+          '**confused**\n'+
+          '**seemslegit**\n'+
+          '**chuckok**\n'+
+          '**pokerface**\n'+
+          '**dude**\n'+
+          '**obvious**\n'+
+          '**iamout**\n'+
+          '**fairenough**\n'+
+          '**facepalm**\n'+
+          '**goodjob**\n'+
+          '**yeah**\n'+
+          '**cool**\n'+
+          '**bplease**\n'+
+          '**wtf**\n'+
+          '**arnielikes**\n'+
+          '**doitnow**\n'+
+          '**what**\n'+
+          '**mickey**\n'+
+          '**attack**\n'+
+          '**sinister**\n'+
+          '**drakeno**\n'+
+          '**xd**\n'+
+          '**smart**\n'+
+          '**mellikes**\n'+
+          '**parkerlikes**\n'+
+          '**angry**\n'+
+          '**pulpfictionlikes**\n'+
+          '**trap**\n'+
+          '**wall**\n'+
+          '**prehistory**\n'+
+          '**throwanother**\n'+
+          '**krillin**\n'+
+          '**keanu**\n'+
+          '**dontwannalive**\n'+
+          '**charliecocaine**\n'+
+          '**trollorstupid**\n'+
+          '**itsatrap**\n'+
+          '**nopower**\n'+
+          '**bsintensifies**\n'+
+          '**pasta**\n'+
+          '**mythbusters**\n'+
+          '**xzibit**\n'+
+          '**10score**\n'+
+          '**doublelikes**\n'+
+          '**genius**\n'+
+          '**sweating**\n'+
+          '**rambolikes**\n'+
+          '**orbital**\n'+
+          '**crossover**\n'+
+          '**offend**\n'+
+          '**doesntaffect**\n'+
+          '**propaganda**\n'+
+          '**babyyeah**\n'+
+          '**wrong**\n'+
+          '**worsttrade**\n'+
+          '**terminatorlikes**\n'+
+          '**dafuq**\n'+
+          '**beating**\n'+
+          '**booze**\n'+
+          '**mcmahon1**\n'+
+          '**mcmahon2**\n'+
+          '**mcmahon3**\n'+
+          '**adapt**\n'+
+          '**evilestthing**\n'+
+          '**laugh**\n'+
+          '**summon**\n'+
+          '**shotgun**\n'+
+          '**barbaric**\n'+
+          '**justdoit**\n'+
+          ;
+          break;
       }
-  
+      
       message.channel.send(linkToPic)
       break;
       
@@ -1798,7 +2224,8 @@ client.on("message", message => {
       '**who**\n'+
       '**whois**\n'+
       '**why**\n'+
-      '**you**\n'
+      '**you**\n'+
+      '**pic [x] : [x] = pic name**\n'
       ;
 
       message.channel.send(responseList)
