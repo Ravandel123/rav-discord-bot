077866d5e768abc40c45801ec9db072a74f28922
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2018-11-19 11:12:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -531,7 +531,7 @@ client.on("message", message => {
       responseList = [
       Rnd(GenerateMultiplier(1000000000)) + '.',
       'This is' + ReturnRandom(additionalList) + Rnd(GenerateMultiplier(1000000000)) + '.',
-      'My calculations shows that this will be' + ReturnRandom(additionalList) + Rnd(GenerateMultiplier(1000000000)) + '.',
+      'My calculations show that this will be' + ReturnRandom(additionalList) + Rnd(GenerateMultiplier(1000000000)) + '.',
       'I think it\'s ' + Rnd(GenerateMultiplier(1000000000)) + '.'
       ];
 
