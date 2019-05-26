a4aebb8b1ceec43793231fdee52a56ef04b30624
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2019-05-26 23:13:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2875,8 +2875,8 @@ client.on("message", message => {
     //----------test--------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------
     case 'test':
-      message.channel.send(TransformToProperEndingApostrophe(everybody))
-      message.channel.send(TransformToProperEndingApostrophe(nobody))
+      message.channel.send(TransformToProperEndingApostrophe('everybody'))
+      message.channel.send(TransformToProperEndingApostrophe('nobody'))
     break;
       
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
